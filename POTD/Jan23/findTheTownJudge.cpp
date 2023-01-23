3a301945071c5494c55b2c661842fc37643ecc82
github_piyush2108 <piyusharmap@gmail.com>
2023-01-23 11:44:06 +0530

    Added potd 23rd jan

---
+++ POTD/Jan23/findTheTownJudge.cpp
@@ -0,0 +1,24 @@
+Date: 23th Jan, 2023
+
+Problem: https://leetcode.com/problems/find-the-town-judge/description/
+
+Solution -->
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+     
+        vector<int> indeg(n+1, 0);
+        vector<int> outdeg(n+1, 0);
+
+        for(int i=0; i<trust.size(); i++){
+            indeg[trust[i][1]]++;
+            outdeg[trust[i][0]]++;
+        }
+
+        for(int i=1;i<=n;i++){
+            if(outdeg[i]==0 && indeg[i]==n-1) return i;
+        }
+
+        return -1;
+    }
+};
