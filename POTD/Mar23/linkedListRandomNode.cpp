6d05d6d1d6254f86c4e8d25183c75789723bd5c0
github_piyush2108 <piyusharmap@gmail.com>
2023-03-10 11:47:55 +0530

    Added potd 10th march

---
+++ POTD/Mar23/linkedListRandomNode.cpp
@@ -0,0 +1,55 @@
+// Date: 10th Mar, 2023
+
+// Problem: https://leetcode.com/problems/linked-list-random-node/
+
+// Solution -->
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+     typedef ListNode Node;
+     Node *head;
+     int n;
+     int len(Node *head)
+     {
+          int l = 0;
+          Node *temp = head;
+          while (temp)
+          {
+               l++;
+               temp = temp->next;
+          }
+          return l;
+     }
+     Solution(Node *root)
+     {
+          head = root;
+          n = len(head);
+     }
+
+     int getRandom()
+     {
+          int random = rand() % n;
+          Node *temp = head;
+          for (int i = 0; i < random; i++)
+          {
+               temp = temp->next;
+          }
+          return temp->val;
+     }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(head);
+ * int param_1 = obj->getRandom();
+ */
